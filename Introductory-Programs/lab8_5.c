b104522558816ab6b9dac354a6cfec73d653acdd
kunalbashist <62015640+kunalbashist@users.noreply.github.com>
2020-10-01 01:20:59 +0530

    Update lab8_5.c
    Completed the code for tower of hanoi

--- Introductory-Programs/lab8_5.c
+++ Introductory-Programs/lab8_5.c
@@ -1,3 +1,25 @@
 //Program to solve the Tower of Hanoi puzzle for n disks
 //Faheem Hassan Zunjani
 
+#include <stdio.h>
+
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) 
+{ 
+    if (n == 1) 
+    { 
+        printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod); 
+        return; 
+    } 
+    towerOfHanoi(n-1, from_rod, aux_rod, to_rod); 
+    printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod); 
+    towerOfHanoi(n-1, aux_rod, to_rod, from_rod); 
+} 
+  
+int main() 
+{ 
+    int n;
+    printf("Enter No. of Disk :- ");
+    scanf("%d", &n);
+    towerOfHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods 
+    return 0; 
+}
